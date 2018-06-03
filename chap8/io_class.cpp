5edc6371a5c56f6536585c13ff5b62d4bbbb9328
kuangtu <kuangtuweb@gmail.com>
2018-06-03 16:51:14 +0800

    chap8

---
+++ chap8/io_class.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::initializer_list;
+using std::ostream;
+using std::istream;
+
+int
+main(int argc, char *argv[]) 
+{
+
+	return 0;
+}
