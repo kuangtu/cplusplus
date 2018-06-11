151d29be5f2bf6c07a0c4dd29aa6731cf597d994
kuangtu <kuangtuweb@gmail.com>
2018-06-11 23:41:39 +0800

    chap9

--- chap9/exec9_13.cpp
+++ chap9/exec9_13.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdbool>
 #include <array>
+#include <list>
 
 using std::cin;
 using std::cout;
@@ -13,6 +14,7 @@ using std::istream;
 using std::vector;
 using std::string;
 using std::array;
+using std::list;
 
 
 int
@@ -20,7 +22,7 @@ main(int argc, char *argv[])
 {
 	vector<int> v1 = {1, 2, 3, 4};
 	vector<int> v2;
-	
+	//直接赋值	
 	v2 = v1;
 
 	vector<int>::iterator begin = v1.begin();
@@ -31,6 +33,37 @@ main(int argc, char *argv[])
 		cout << *begin << endl;
 		++begin;
 	}
+
+	printf("%s----%s-----%d\n", __FILE__, __FUNCTION__, __LINE__);
+
+	begin = v2.begin();
+	end = v2.end();
+
+	while(begin != end)
+	{
+		cout << *begin << endl;
+		++begin;
+	}
+
+	list<int> listints = {1, 2, 3, 4 };
+	list<int>::iterator begin1 = listints.begin();
+	list<int>::iterator end1 = listints.end();
+	vector<double> vectordouble(begin1, end1);
+
+	vector<double>::iterator begin2 = vectordouble.begin();
+	vector<double>::iterator end2 = vectordouble.end();
+
+	while(begin2 != end2)
+	{
+		cout << *begin2 << endl;
+		++begin2;
+	}
+
+
+
+
+
+
 
 	return 0;
 }
