1d797d562a9b6623f44a0fe42aef63a2e665ccd9
kuangtu <kuangtuweb@gmail.com>
2018-06-28 23:08:17 +0800

    chap12 new

--- chap12/new_ptr.cpp
+++ chap12/new_ptr.cpp
@@ -28,6 +28,22 @@ main(int argc, char *argv[])
 	auto pp1 = new auto(c);
 
 	cout << "pp1:" << *pp1 << endl;
+
+	//分配const对象
+	
+	const int *pci = new const int(1000);
+
+	const int *ptmp = pci;
+
+	cout << *ptmp << endl;
+
+	pci = new const int(2000);
+
+	cout << *pci << endl;
+
+	//指向const int，不能改变
+	//*pci = 200;
+	
 	return 0;
 }
 
