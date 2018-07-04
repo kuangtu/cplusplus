4452294c6a80bfa8ef344cfb937b5cd74ddcbfb2
kuangtu <kuangtuweb@gmail.com>
2018-07-04 23:40:32 +0800

    chap13

--- chap13/sales.cpp
+++ chap13/sales.cpp
@@ -2,11 +2,15 @@
 #include <string>
 
 using std::string;
+using std::cout;
+using std::endl;
 
 class Sales_data {
 	public:
 		Sales_data() = default;
 		Sales_data(string no, int units, double price): bookNo(no), units_sold(units), revenue(price) { }
+		//拷贝构造函数
+		Sales_data(const Sales_data &orig): bookNo(orig.bookNo), units_sold(orig.units_sold), revenue(orig.revenue) { }
 		string getBookNo() {
 			return bookNo;
 		}
@@ -25,6 +29,10 @@ int
 main(int argc, char *argv[]) 
 {
 	Sales_data data1("0001", 100, 10.1);
+	Sales_data data2(data1);
+
+	cout << data2.getRevenue() << endl;
+
 	return 0;
 }
 
