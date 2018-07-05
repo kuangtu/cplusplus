396a76a2219a76177962c5d5526441deef3dd756
kuangtu <kuangtuweb@gmail.com>
2018-07-05 23:59:45 +0800

    chap13

--- chap13/pointer_class.cpp
+++ chap13/pointer_class.cpp
@@ -7,13 +7,20 @@ using std::endl;
 
 class HasPtr {
 	public:
-		HasPtr(const string &s = string()):ps(new string(s)), i(0) { }
-		HasPtr(const HasPtr &p):ps(new string(*p.ps)), i(p.i) { }
+		HasPtr(const string &s = string()):ps(new string(s)), i(0), use(new size_t(1)) { }
+		HasPtr(const HasPtr &p):ps(new string(*p.ps)), i(p.i), use(p.use) { ++*use;}
 		HasPtr& operator=(const HasPtr &);
-		~HasPtr() { delete ps; }
+		~HasPtr() { 
+			if (--*use == 0)
+			{
+				delete ps;
+				delete use;
+			}
+		}
 	private:
 		string *ps;
 		int i;
+		size_t *use;
 };
 
 HasPtr& HasPtr::operator=(const HasPtr &rhs)
@@ -33,6 +40,8 @@ int
 main(int argc, char *argv[]) 
 {
 	HasPtr p1("test");
+
+	//赋值或者拷贝的时候,改变引用计数
 	HasPtr p2 = p1; 
 
 	return 0;
