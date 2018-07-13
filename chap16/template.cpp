07ce1b0feac332a95e55abd835c67c0ec4570ce6
kuangtu <kuangtuweb@gmail.com>
2018-07-13 23:12:11 +0800

    chap16

---
+++ chap16/template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template <typename T>
+int compare(const T &v1, const T &v2)
+{
+	if (v1 < v2) return -1;
+	if (v2 < v1) return 1;
+
+	return 0;
+}
+
+int
+main(int argc, char *argv[]) 
+{
+	cout << compare(1, 0) << endl;
+	return 0;
+}
