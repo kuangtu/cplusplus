675f3150d23e25d2a921fc46f5393a2792728368
kuangtu <kuangtuweb@gmail.com>
2020-03-07 15:14:13 +0800

    chap2

---
+++ chap2/exec2_8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int
+main(int argc, char *argv[]) 
+{
+
+	std::cout << "2M";
+	std::cout << "\n";
+	
+	std::cout << "2";
+	std::cout << "\t";
+	std::cout << "M";
+	std::cout << "\n";
+	return 0;
+}
+
